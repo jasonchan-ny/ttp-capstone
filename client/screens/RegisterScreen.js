1b779f51f8ad5026acfe939850fe55558807604c
Vin Ndokaj <vinndokaj@gmail.com>
2021-01-25 13:28:37 -0500

    naming conventions and more dependencies

---
+++ client/screens/RegisterScreen.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react'
+import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native'
+
+export default function RegisterScreen() {
+    const [username, setUsername] = useState('username');
+    const [password, setPassword] = useState('password');
+
+    return (
+        <View style={styles.container}>
+            <Text>Login or Register Below!</Text>
+            <TextInput 
+                style={styles.input}
+                onChangeText={user => setUsername(user)}
+                value={username}
+            />
+            <TextInput
+                style={styles.input}
+                onChangeText={pass => setPassword(pass)}
+                value={password}
+            />
+            <TouchableOpacity>
+                <Text>Register</Text>
+            </TouchableOpacity>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    input: {
+
+    },
+    login: {
+
+    },
+    register: {
+
+    }
+  });
