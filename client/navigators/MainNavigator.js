558bee9e9d10d018141f4f6a35adfbb6359f3bf6
Vin Ndokaj <vinndokaj@gmail.com>
2021-01-26 18:27:18 -0500

    changed tab navigator to enable swiping between screens

--- client/navigators/MainNavigator.js
+++ client/navigators/MainNavigator.js
@@ -1,38 +1,54 @@
 import React from "react";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { AntDesign } from '@expo/vector-icons';
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+import { AntDesign } from "@expo/vector-icons";
 
 import { HistoryScreen, TimerScreen, NewWorkoutScreen } from "../screens";
 
 export default function MainNavigator() {
-  const Tab = createBottomTabNavigator();
+  const Tab = createMaterialTopTabNavigator();
 
   return (
     <Tab.Navigator
-      screenOptions={({ route }) => ({
-        tabBarIcon: ({ focused, color, size }) => {
-          let iconName;
-
-          if (route.name === "History") {
-            iconName = "linechart";
-          } else if (route.name === "Timer") {
-            iconName = focused ? "clockcircle" : "clockcircleo";
-          } else if (route.name === "New Workout") {
-            iconName = focused ? "pluscircle" : "pluscircleo";
-          }
-
-          // You can return any component that you like here!
-          return <AntDesign name={iconName} size={size} color={color} />;
-        },
-      })}
       tabBarOptions={{
         activeTintColor: "tomato",
         inactiveTintColor: "gray",
+        showIcon: true,
       }}
+      tabBarPosition="bottom"
     >
-      <Tab.Screen name="History" component={HistoryScreen} />
-      <Tab.Screen name="Timer" component={TimerScreen} />
-      <Tab.Screen name="New Workout" component={NewWorkoutScreen} />
+      <Tab.Screen
+        name="History"
+        component={HistoryScreen}
+        options={{
+          tabBarIcon: ({ focused, color }) => (
+            <AntDesign name={"linechart"} size={20} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Timer"
+        component={TimerScreen}
+        options={{
+          tabBarIcon: ({ focused, color }) =>
+            focused ? (
+              <AntDesign name={"clockcircle"} size={20} color={color} />
+            ) : (
+              <AntDesign name={"clockcircleo"} size={20} color={color} />
+            ),
+        }}
+      />
+      <Tab.Screen
+        name="New Workout"
+        component={NewWorkoutScreen}
+        options={{
+          tabBarIcon: ({ focused, color }) =>
+            focused ? (
+              <AntDesign name={"pluscircle"} size={20} color={color} />
+            ) : (
+              <AntDesign name={"pluscircleo"} size={20} color={color} />
+            ),
+        }}
+      />
     </Tab.Navigator>
   );
 }
